e4a19efd2df88998a523f02857be119d87cad4aa
huangshaohua <huangshaohua945@126.com>
2020-08-18 09:25:26 +0800

    修改配置文件

--- SAND-Base/ls-zookeeper/src/main/resources/zooKeeper.bat
+++ SAND-Base/ls-zookeeper/src/main/resources/zooKeeper.bat
@@ -1,3 +1,3 @@
-start /b C:\HuangShaohua\DevTools\zookeeper\zoo-monkey\bin\zkServer.cmd
-start /b C:\HuangShaohua\DevTools\zookeeper\zoo-pig\bin\zkServer.cmd
-start /b C:\HuangShaohua\DevTools\zookeeper\zoo-tiger\bin\zkServer.cmd
+start /b C:\Work\devTools\zookeeper\zoo-monkey\bin\zkServer.cmd
+start /b C:\Work\devTools\zookeeper\zoo-pig\bin\zkServer.cmd
+start /b C:\Work\devTools\zookeeper\zoo-tiger\bin\zkServer.cmd
