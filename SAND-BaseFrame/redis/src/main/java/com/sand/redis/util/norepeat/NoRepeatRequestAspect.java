b57117fd06c54342b2e1b4c05acea09abba14dbd
huangsh <huangshaohua945@126.com>
2022-03-29 16:48:00 +0800

    防重复提交

--- SAND-BaseFrame/redis/src/main/java/com/sand/redis/util/norepeat/NoRepeatRequestAspect.java
+++ SAND-BaseFrame/redis/src/main/java/com/sand/redis/util/norepeat/NoRepeatRequestAspect.java
@@ -1,6 +1,7 @@
 package com.sand.redis.util.norepeat;
 
 import com.alibaba.fastjson.JSONObject;
+import com.sand.core.exception.BusinessException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,6 +10,8 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DigestUtils;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -16,8 +19,10 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * 功能说明：防重复提交切面类 <br>
@@ -28,8 +33,26 @@ import java.util.concurrent.TimeUnit;
 @Aspect
 @Component
 public class NoRepeatRequestAspect {
-    @Autowired
     private RedisTemplate redisTemplate;
+    /**
+     * <p>
+     * 功能描述：进行序列化（防止key乱码）
+     * </p>
+     * 开发人员：@author shaohua.huang
+     * 开发时间：2022/3/29 16:39
+     * 修改记录：新建
+     *
+     * @param redisTemplate redisTemplate
+     */
+    @Autowired(required = false)
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        this.redisTemplate = redisTemplate;
+    }
 
     /**
      * 定义切点
@@ -64,9 +87,9 @@ public class NoRepeatRequestAspect {
         // 获取UUID以及方法标记，生成redisKey和redisValue
         String uuid = request.getHeader(NoRepeatRequestConstant.UUID);
         String redisKey = NoRepeatRequestConstant.NO_REPEAT_REQUEST_PREFIX
-                .concat(uuid)
+                .concat(uuid).concat("$")
                 .concat(getMethodSign(method, joinPoint.getArgs()));
-        String redisValue = redisKey.concat(annotation.value()).concat("submit duplication");
+        String redisValue = redisKey.concat(annotation.value()).concat(" submit duplication");
 
         if (!redisTemplate.hasKey(redisKey)) {
             // 设置防重复请求限时标记（前置通知）
@@ -77,10 +100,10 @@ public class NoRepeatRequestAspect {
             } catch (Throwable throwable) {
                 // 确保方法执行异常实时释放限时标记（异常后置通知）
                 redisTemplate.delete(redisKey);
-                throw new RuntimeException(throwable);
+                throw new Exception(throwable);
             }
         } else {
-            throw new RuntimeException("请勿重复提交！");
+            throw new BusinessException("请勿重复提交！");
         }
     }
 
@@ -94,14 +117,13 @@ public class NoRepeatRequestAspect {
      *
      * @param method method
      * @param args   args
-     * @return java.lang.String 签名后的请求参数
+     * @return java.lang.String 40位的签名串，例：30c9b14ee8b282f12009e0b2b06542145f263e4f
      */
     private String getMethodSign(Method method, Object... args) {
-        StringBuilder sb = new StringBuilder(method.toString());
-        for (Object arg : args) {
-            sb.append(toString(arg));
-        }
-        return DigestUtils.sha1DigestAsHex(sb.toString());
+        String sb = Arrays.stream(args)
+                .map(this::toString)
+                .collect(Collectors.joining("", method.toString(), ""));
+        return DigestUtils.sha1DigestAsHex(sb);
     }
 
     /**
